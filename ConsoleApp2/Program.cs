0df09ac0b7056a9a3b18dd9d31e508495ee12912
denismixy <psp3000d@mail.ru>
2022-02-19 21:25:01 +0500

    Change architecture

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lrNet1 {
     class Program {
         static void Main(string[] args) {
-            Car car = new Car();
+            
         }
     }
 }
--- ConsoleApp2/VisitLogger.cs
+++ ConsoleApp2/VisitLogger.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace lrNet1 {
     class VisitLogger {
+        private List<Visit> visits;
+        private Visit visitForEdit;
+
+        public void AddVisit(string fio = null, string carBrand = null, string carNumber = null, string carModel = null,
+                            string startParkingDate = null, string endParkingDate = null, string parkingPlace = null, string cost = null) {
+            Visit visit = new Visit(fio, carBrand, carNumber, carModel, startParkingDate, endParkingDate, parkingPlace, cost);
+            visits.Add(visit);
+        }
+
+        public void ChoiceVisitForChange(string fio = null, string carBrand = null, string carNumber = null, string carModel = null,
+                            string startParkingDate = null, string endParkingDate = null, string parkingPlace = null, string cost = null) {
+            visitForEdit = new Visit(fio, carBrand, carNumber, carModel, startParkingDate, endParkingDate, parkingPlace, cost);
+        }
+
+        public void DeleteVisit() {
+            visits.RemoveAll(element => element.Equals(visitForEdit));
+        }
+
+        public void EditVisit(string fio = null, string carBrand = null, string carNumber = null, string carModel = null,
+                            string startParkingDate = null, string endParkingDate = null, string parkingPlace = null, string cost = null) {
+            Visit visit = new Visit(fio, carBrand, carNumber, carModel, startParkingDate, endParkingDate, parkingPlace, cost);
+            visits[visits.IndexOf(visitForEdit)] = visit;
+        }
+
+        public List<Visit> GetAllVisits() {
+            return new List<Visit>(visits);
+        }
+
+        public List<Visit> GetVisitsByClient(string fio) {
+            return visits.Where(element => element.Fio == fio).ToList();
+        }
+
     }
 }
